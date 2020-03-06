95a82a02a60d51fde4c7fc4b1ba08828f3fc4a21
Amber <amber.ramey.2022@student.lhschools.org>
2020-03-06 12:07:08 -0500

    intelij commit

---
+++ src/com/LickingHeight/Main.java
@@ -0,0 +1,145 @@
+package com.LickingHeight;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Phase one
+        //Declare all of the variables that you are going to use
+        String noun;  //Declared a variable
+        String describingWord;
+        String favoritePlaceToGo;
+        String adjetive;
+        String color;
+        String name;
+        String superPower;
+        String favoritePhrase;
+        String object;
+        String wordEndingInIng;
+        String weapon;
+        String actionWord;
+        String wordEndingInY;
+        String favoriteFood;
+        String number;
+        String favoriteObject;
+        String aGame;
+        String job;
+        String pluralForAnAnimal;
+        String favoriteThingToDo;
+
+
+
+
+
+
+
+        // Phase two
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+
+        System.out.println("Give me a random animal.");
+        noun = keyboard.nextLine();
+
+        System.out.println("Whats a word that describes size?");
+        describingWord = keyboard.nextLine();
+
+        System.out.println("Dream vacation spot to visit");
+        favoritePlaceToGo = keyboard.nextLine();
+
+        System.out.println("Give me a word that describes a personality");
+        adjetive = keyboard.nextLine();
+
+        System.out.println("What is your favorite color?");
+        color = keyboard.nextLine();
+
+        System.out.println("Give me a random name");
+        name = keyboard.nextLine();
+
+        System.out.println("If you could hve one super power what would it be?");
+        superPower = keyboard.nextLine();
+
+        System.out.println("Action word for movement");
+        actionWord = keyboard.nextLine();
+
+        System.out.println("Whats your favorite phrase?");
+        favoritePhrase = keyboard.nextLine();
+
+        System.out.println("Think of an object");
+        object = keyboard.nextLine();
+
+        System.out.println("What is your favorite number?");
+        number = keyboard.nextLine();
+
+        System.out.println("Whats one thing you cant live without? (Put a S at the end) ");
+        favoriteObject = keyboard.nextLine();
+
+        System.out.println("Give me a word that ends in 'Ing' ");
+        wordEndingInIng = keyboard.nextLine();
+
+        System.out.println("Think of a weapon?");
+        weapon = keyboard.nextLine();
+
+        System.out.println("Give me a word ending in 'y'");
+        wordEndingInY = keyboard.nextLine();
+
+        System.out.println("What is your favorite food?");
+        favoriteFood = keyboard.nextLine();
+
+        System.out.println("What is a game that you usually play?");
+        aGame = keyboard.nextLine();
+
+        System.out.println("Name a job");
+        job = keyboard.nextLine();
+
+        System.out.println("Give me a plurl name for an animal");
+        pluralForAnAnimal = keyboard.nextLine();
+
+        System.out.println("What is your favorite thing to do?");
+        favoriteThingToDo = keyboard.nextLine();
+
+        //Phase three
+        System.out.println("Once upon a time there was" + " \n" +
+                "a " + noun + " with a " + describingWord + " \n" +
+                "head who tried to attack " + favoritePlaceToGo + "." + " \n"+
+                "Until a " + adjetive + " hero named " + "\n" +
+                color + " "+ name + " whose superpower is " + superPower + ", Found"+" \n" +
+                "the monster and yelled " + "'"+ favoritePhrase + "'" + ". The hero" + " \n" +
+                "used a/an " + object + " to defeat the " + " \n"+
+                "monster but the monster had a "+ wordEndingInIng +" "+ weapon+ ". The Monster " + "\n" +
+                 actionWord +" away with some "+ wordEndingInY +" "+ favoriteFood + "." + "\n"+
+                "The hero defeated the monster so the citizens thanked " +"\n"+
+                "him by giving him "+ number+" "+ favoriteObject +". After that some of " + "\n"+
+                "the citizens went back to playing "+ aGame+ ", others went back " + "\n"+
+                "to being a "+ job+ ", The Children went back to " + "\n" +
+                "catching "+ pluralForAnAnimal+ " and the hero went " + "\n"+
+                "back to " + favoriteThingToDo);
+
+
+
+
+
+                //Ask for input from the user to initialize all of the previously declared varibles
+        //Phase three
+        //print entire story, without having it run off the page
+        //- single comment
+        /* block comment*/
+        // phase one
+        // all of phase one
+
+        //phase two
+        //all of code for phase two
+
+        //phase three
+        // all of phase three
+
+        // 20 variables- new variables that are descriptively named
+        //Declared a variable
+        //initialized a variable
+
+        //declare and initialize on the same line
+
+    }
+
+}
